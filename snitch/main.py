771155b7737d64da3a1535589926237ff6efc356
timehollyname <time.holly.name@gmail.com>
2026-02-15 18:49:33 +0300

    update/timehollyname

--- snitch/main.py
+++ snitch/main.py
@@ -26,6 +26,8 @@ if __name__ == "__main__":
                 c.add(Connection(ip=ip, connected_at=int(time())))
 
         for connection in c.to_list():
+            connection.duration = int(time()) - connection.connected_at
+
             if connection.name is None:
                 name: str | None = a.detect(ip)
 
